97e9c6194d7945426275c7cb8acd1220140d131a
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-01 14:33:03 +0100

    feat: add basic fetch for mockup-data

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,35 @@
 import Head from "next/head";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../organisms/layout";
 
+import useGet from "../ions/hooks/fetch/get";
+
+import { DataGrid } from "@mui/x-data-grid"
+
+
 const Page = () => {
+	const { data, loading, error } = useGet("http://0.0.0.0:3000/api/testCoins");
+
+	useEffect(()=>{
+		fetch("/src/pages/api/test-coins")
+			.then(response => console.log(response))
+
+	},[])
 	return (
 		<Layout>
 			<Head>
 				<title key="title">coin ghost</title>
 				<meta key="description" name="description" content="This is my project" />
 			</Head>
+			{loading && <div>Loading...</div>}
+			{error && <div>{error.message}</div>}
+			{data && (
+				<pre>
+					<code>{JSON.stringify(data, null, 4)}</code>
+				</pre>
+			)}
 		</Layout>
+
 	);
 };
 

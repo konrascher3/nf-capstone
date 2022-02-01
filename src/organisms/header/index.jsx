92ffb12cefef4339f495591c723bfb238625a3f1
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-01 11:07:02 +0100

    feat: add simple header

--- src/organisms/header/index.jsx
+++ src/organisms/header/index.jsx
@@ -1,9 +1,24 @@
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+
+// Import logo
+import logoDarkMode from "/src/ions/img/logos/complete-logo-dark-mode.svg"
+
+// MUI Imports
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
 
 const Header = () => {
 	return (
 		<header>
+			<Box sx={{flexGrow: 1}}>
+				<AppBar position="static">
+					<Toolbar>
+						<img style={{height: "40px"}} src={logoDarkMode.src} alt="Coin ghost logo" />
+					</Toolbar>
+				</AppBar>
+			</Box>
 			<nav data-test-id="navigation">
 				<Link href="/">Home</Link>
 				<Link href="/about">About me</Link>

730766e6a7a91cc006804c338f624e63786344c1
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-01 11:46:08 +0100

    chore: cypress-folder

--- cypress/integration/common/assertions.js
+++
@@ -1,6 +0,0 @@
-import { Then } from "cypress-cucumber-preprocessor/steps";
-import { pages } from "../helpers";
-
-Then(/^the "([^"]*)" page is visible$/, function (page) {
-	cy.url().should("include", pages[page]);
-});
--- cypress/integration/helpers/index.js
+++
@@ -1,6 +0,0 @@
-export const pages = {
-	root: "/",
-	about: "/about",
-};
-
-export const dataTestId = name => `[data-test-id="${name}"]`;

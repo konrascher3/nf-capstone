68de853789ff522a1d41fe4fe0d4bc542c280e26
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-02 16:09:01 +0100

    chore: remove unused import

--- src/molecules/tab-bar/TabBar.jsx
+++ src/molecules/tab-bar/TabBar.jsx
@@ -51,42 +51,37 @@ const TabBar = () => {
 		<Box sx={{ width: '100%' }}>
 			<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
 				<Tabs
+					// TODO: Add breakpoint-switch for centered and varant="scrollable"
+					// centered
 					value={value}
 					aria-label="Coin categories"
-					onChange={handleChange}
 					variant="scrollable"
 					scrollButtons="auto"
+					onChange={handleChange}
 				>
 					{/* Custom Categories */}
-					<Tab value="show_top_coins" label="Top Coins 👑" {...a11yProps("show_top_coins")} />
-					{/* Fetches coins-data based on Mcap-rank */}
-					{/*https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false*/}
-					{/* Returns API-Endpoint "/coins/markets" and params "target-currency" and "sort-order" */}
+					<Tab label="Top Coins 👑" {...a11yProps("show_top_coins")} />
 
-					<Tab value="show_gainers_losers" label="Gainers & Losers" {...a11yProps("show_gainers_losers")} />
+					{/* TODO: Requires custom filter; build custom dataset; develop a proxy */}
+					{/*<Tab label="Gainers & Losers" {...a11yProps("show_gainers_losers")} />*/}
 					{/* Fetches two coins-data-sets based on Mcap-rank ascending and descending*/}
 					{/*https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false*/}
 					{/* Returns API-Endpoint "/coins/markets" and params "target-currency" and "sort-order" */}
 
-					<Tab value="show_trending" label="Trending 🔥" {...a11yProps("show_trending")} />
+					{/*<Tab label="Trending 🔥" {...a11yProps("show_trending")} />*/}
 					{/* Fetches coin-IDs based on coinGecko-trending */}
 					{/* https://api.coingecko.com/api/v3/search/trending */}
 					{/* Coin-IDs need to be mapped against "/coins/markets" to get coin-data*/}
 
 					{/* Sectors */}
-					{/* Fetches coins-data based on Mcap-rank and Category-ID */}
-					{/* Category-ID:  e.g.:"stablecoins"*/}
-					{/*https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false*/}
-					{/* Returns API-Endpoint "/coins/markets" and params "target-currency", "sort-order", and "category-id" */}
-					<Tab value="show_stablecoins" label="Stablecoins" {...a11yProps("show_stablecoins")} />
-					<Tab value="show_meme_coins" label="Meme coins" {...a11yProps("show_meme_coins")} />
-					<Tab value="show_nfts" label="NFTs" {...a11yProps("show_nfts")} />
-					<Tab value="show_dex_tokens" label="DEX Tokens" {...a11yProps("show_dex_tokens")} />
-					<Tab value="show_game_fi" label="Game Fi" {...a11yProps("show_game_fi")} />
-					<Tab value="show_music" label="Music" {...a11yProps("show_music")} />
+					<Tab label="Stablecoins" {...a11yProps("show_stablecoins")} />
+					<Tab label="Meme coins" {...a11yProps("show_meme_coins")} />
+					<Tab label="NFTs" {...a11yProps("show_nfts")} />
+					<Tab label="DEX Tokens" {...a11yProps("show_dex_tokens")} />
+					<Tab label="Game Fi" {...a11yProps("show_game_fi")} />
+					<Tab label="Music" {...a11yProps("show_music")} />
 				</Tabs>
 			</Box>
-			{console.log(value)}
 		</Box>
 	);
 }

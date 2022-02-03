f2df3115486f19eb6f69eafe190eabc7f7182093
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-03 17:06:16 +0100

    feat: add menu-button

--- src/organisms/header/index.jsx
+++ src/organisms/header/index.jsx
@@ -8,13 +8,19 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 const Header = () => {
 	return (
 		<header>
 			<Box sx={{flexGrow: 1}}>
 				<AppBar position="static" elevation={1}>
-					<Toolbar>
+					<Toolbar sx={{ display: "flex", justifyContent: "space-between", alignItems: "center"}}>
 						<img style={{width: "180px"}} src={logoDarkMode.src} alt="Coin ghost logo" />
+						<IconButton>
+							<MenuIcon />
+						</IconButton>
 					</Toolbar>
 				</AppBar>
 			</Box>

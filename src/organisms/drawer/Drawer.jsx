d394b0032be3a4053e69bb78fddef9df27af6ed1
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-03 19:28:42 +0100

    chore: refactor components

--- src/organisms/drawer/Drawer.jsx
+++ src/organisms/drawer/Drawer.jsx
@@ -3,13 +3,9 @@ import React from "react"
 // MUI Imports
 import Box from "@mui/material/Box"
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider';
 
-import ExploreIcon from '@mui/icons-material/Explore';
+// Custom-components Imports
+import DrawerList from "/src/molecules/drawer-list/DrawerList"
 
 // useStore
 import useStore from "/src/ions/hooks/state/useStore"
@@ -17,26 +13,16 @@ import useStore from "/src/ions/hooks/state/useStore"
 
 const Drawer = () => {
 	const { open, setOpen } = useStore((state) => state)
-	console.log(open)
 
 	return (
 		<SwipeableDrawer
 			anchor="right"
 			open={open}
-			onOpen={()=>{}}
+			onOpen={()=>{setOpen(!open)}}
 			onClose={()=>{setOpen(!open)}}
 		>
 			<Box>
-				<List>
-					<ListItem button>
-						<ListItemIcon><ExploreIcon /></ListItemIcon>
-						<ListItemText>Explore</ListItemText>
-					</ListItem>
-					<Divider />
-					<ListItem button>
-						<ListItemText>Watchlist</ListItemText>
-					</ListItem>
-				</List>
+				<DrawerList />
 			</Box>
 		</SwipeableDrawer>
 	)

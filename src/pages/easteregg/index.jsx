28376c607726b99e35cc4cfa9b11e515f14d29f7
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-04 01:56:58 +0100

    feat: implement a little easter-egg 🐰

---
+++ src/pages/easteregg/index.jsx
@@ -0,0 +1,36 @@
+import Box from "@mui/material/Box";
+import Head from "next/head";
+import React from "react";
+import Layout from "/src/organisms/layout/index";
+import Image from "next/image";
+
+// useStore
+import useStore from "/src/ions/hooks/state/useStore"
+
+// Import meme
+import meme from "/src/ions/img/memes/jurassic-park.webp"
+
+
+const Page = () => {
+	const { coins, loading, error } = useStore((state) => state);
+
+	return (
+		<Layout>
+			<Head>
+				<title key="title">coin ghost</title>
+				<meta key="description" name="description" content="This is my project" />
+			</Head>
+			{loading && <div>Loading...</div>}
+			{error && <div>{error.message}</div>}
+			{coins && (
+				<div>
+					<Box sx={{ display: "flex", m: 5, alignItems: "center", justifyContent: "center" }}>
+						<Image layout="intrinsic" placeholder="blur" src={meme} alt="Ah ah ah! You didn't say the magic word!"/>
+					</Box>
+				</div>
+			)}
+		</Layout>
+	);
+};
+
+export default Page;

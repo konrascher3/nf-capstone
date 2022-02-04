9851773d06af45f02bb90ee860e5dbcccff1c03a
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-04 13:46:20 +0100

    feat: implement custom-checkbox component

---
+++ src/atoms/custom-checkbox/CustomCheckbox.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+// MUI Import
+import Box from "@mui/material/Box";
+
+// useStore
+import useStore from "/src/ions/hooks/state/useStore";
+
+
+const CustomCheckbox = ({ params  }) => {
+	const { meta, toggleFavorited } = useStore((state) => state);
+	const metaData = meta[params.id];
+	const favorited = metaData?.favorited;
+	return (
+		<label>
+			{/*Checkbox-component*/}
+			<input
+				type="checkbox"
+				style={{
+					backgroundColor: "green",
+					position: "fixed",
+					top: "-100%",
+					left: "-100vw"
+				}} onChange={()=>{
+				toggleFavorited(params.id)
+				// console.log(meta[params.id].favorited)
+				}}
+			/>
+			{/*Increase tap-target*/}
+			<div style={{
+				height: 51,
+				width: 51,
+				position: "absolute",
+				bottom: -16,
+				left: -10 }}
+			>
+				{/*Represents the custom-checkbox*/}
+				<Box sx={{
+					height:51,
+					width: 5,
+					backgroundColor: favorited ? "#BD93F9" : "#BD93F966",
+					position: "absolute",
+					top: -.5 }}
+				/>
+			</div>
+		</label>
+	)
+}
+
+export default CustomCheckbox

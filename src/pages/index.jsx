daef58b573fdb1a9bb1f27f14d2170d70d65ba75
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-05 11:52:21 +0100

    fix: fix dependency-bug

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -26,7 +26,7 @@ const {data, error, fetchData, coins} = useStore((state) => state);
 // Reset coins-array
 	useEffect(()=>{
 		fetchData("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
-	}, [])
+	}, [fetchData])
 
 	return (
 		<Layout>

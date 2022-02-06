9b9b52a825fc651014f3a7ce0462bac78074d6df
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-06 15:35:53 +0100

    chore: refactor marquee-component

--- src/atoms/marquee-content/MarqueeContent.jsx
+++ src/atoms/marquee-content/MarqueeContent.jsx
@@ -1,3 +1,4 @@
+import Box from "@mui/material/Box";
 import React from "react"
 
 import Typography from  "@mui/material/Typography"
@@ -13,16 +14,56 @@ const marqueeContent = () => {
 			{data &&
 				<>
 					<Typography variant="caption">{`Coins: ${data.Coins.toString()}`}</Typography>
-					<Typography variant="caption">⠀•⠀</Typography>
-					<Typography variant="caption">{`Exchanges: ${data.Exchanges.toString()}`}</Typography>
-					<Typography variant="caption">⠀•⠀</Typography>
-					<Typography variant="caption">{`Dominance: BTC ${data.dominance.BTC.toString()}% ETH: ${data.dominance.ETH.toString()}%`}</Typography>
-					<Typography variant="caption">⠀•⠀</Typography>
-					<Typography variant="caption">{`Total Mcap: ${new Intl.NumberFormat("en-US", {notation: "compact", style: 'currency', currency: 'USD'}).format(data["Market Cap"][0].toString())}`}</Typography>
-					<Typography variant="caption"> (<span style={data["Market Cap"][1] > 0 ? {color: "green"} : {color: "red"}}>{data["Market Cap"][1].toString()}%</span>)</Typography>
-					<Typography variant="caption">⠀•⠀</Typography>
-					<Typography variant="caption">{`24h Vol: ${new Intl.NumberFormat("en-US", {notation: "compact", style: 'currency', currency: 'USD'}).format(data["24h Vol"].toString())}`}</Typography>
-					<Typography variant="caption">⠀•⠀</Typography>
+					<Typography variant="caption">
+						<Box sx={{ ml: .75, mr: .75 }}>
+							<Typography variant="caption" color="text.secondary">
+								•
+							</Typography>
+						</Box>
+					</Typography>
+					<Typography variant="caption">
+						{`Exchanges: ${data.Exchanges.toString()}`}
+					</Typography>
+					<Typography variant="caption">
+						<Box sx={{ ml: .75, mr: .75 }}>
+							<Typography variant="caption" color="text.secondary">
+								•
+							</Typography>
+						</Box>
+					</Typography>
+					<Typography variant="caption">
+						{`Dominance: BTC ${data.dominance.BTC.toString()}% ETH: ${data.dominance.ETH.toString()}%`}
+					</Typography>
+					<Typography variant="caption">
+						<Box sx={{ ml: .75, mr: .75 }}>
+							<Typography variant="caption" color="text.secondary">
+								•
+							</Typography>
+						</Box>
+					</Typography>
+					<Typography variant="caption">
+						{`Total Mcap: ${new Intl.NumberFormat("en-US", {notation: "compact", style: 'currency', currency: 'USD'}).format(data["Market Cap"][0].toString())}`}
+					</Typography>
+					<Typography variant="caption">
+						(<span style={data["Market Cap"][1] > 0 ? {color: "green"} : {color: "red"}}>{data["Market Cap"][1].toString()}%</span>)
+					</Typography>
+					<Typography variant="caption">
+						<Box sx={{ ml: .75, mr: .75 }}>
+							<Typography variant="caption" color="text.secondary">
+								•
+							</Typography>
+						</Box>
+					</Typography>
+					<Typography variant="caption">
+						{`24h Vol: ${new Intl.NumberFormat("en-US", {notation: "compact", style: 'currency', currency: 'USD'}).format(data["24h Vol"].toString())}`}
+					</Typography>
+					<Typography variant="caption">
+						<Box sx={{ ml: .75, mr: .75 }}>
+							<Typography variant="caption" color="text.secondary">
+								•
+							</Typography>
+						</Box>
+					</Typography>
 				</>}
 		</>
 	)

73e4361639cf1716b57db5e5e6268648b74b088a
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-06 17:19:20 +0100

    chore: add CssBaseline

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,10 +2,14 @@ import { Global } from "@emotion/react";
 import React from "react";
 import { globalStyle } from "../ions/styles";
 
+import CssBaseline from '@mui/material/CssBaseline';
+
+
 const App = ({ Component, pageProps }) => {
 	return (
 		<>
 			<Global styles={globalStyle} />
+			<CssBaseline />
 			<Component {...pageProps} />
 		</>
 	);

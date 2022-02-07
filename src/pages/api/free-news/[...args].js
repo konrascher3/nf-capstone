0008b1cc288a74afb8ca3c784140d86edbce76a7
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-07 14:56:56 +0100

    feat: add api-route for news with caching

---
+++ src/pages/api/free-news/[...args].js
@@ -0,0 +1,40 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// TODO: Refactor caching w. https://www.npmjs.com/package/lru-cache
+import axios from "axios";
+import path from "node:path";
+
+const REFRESH_INTERVAL = 1000 * 60 * 60;
+
+const cache = {}
+const handler = async (request, response) => {
+	const { args, ...params } = request.query
+	const slug = args.join("/")
+
+	const host = "free-news.p.rapidapi.com"
+	const endpoint = path.join(host, slug)
+	const url = `https://${endpoint}`
+
+	const options = {
+		params,
+		headers: {
+			"x-rapidapi-host": "free-news.p.rapidapi.com",
+			"x-rapidapi-key": process.env.X_RAPIDAPI_KEY
+		}
+	}
+
+	if(cache[url]){
+		console.log(`Getting data from cache on ${new Date().toISOString()}`)
+		response.status(200).json(cache[url]);
+	} else {
+		console.log(`Fetching new data from API on ${new Date().toISOString()}`)
+		const { data } = await axios.get(url, options);
+		const fullData = { data, meta: { updatedOn: new Date().toISOString() } }
+		cache[url] = fullData
+		setTimeout(() => {
+			cache[url] = null
+		}, REFRESH_INTERVAL)
+		response.status(200).json(fullData);
+	}
+}
+
+export default handler

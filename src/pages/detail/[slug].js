d58c5476cb12c312be9034aefef97bcc0e7d3d15
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-08 12:07:56 +0100

    chore: refactor detail-page

--- src/pages/detail/[slug].js
+++ src/pages/detail/[slug].js
@@ -1,16 +1,32 @@
+import Box from "@mui/material/Box";
 import Head from "next/head";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "/src/organisms/layout/index";
 import useGet from "/src/ions/hooks/fetch/get";
 
 import { useRouter } from "next/router"
+import FastMarquee from "../../molecules/fast-marquee/FastMarquee";
+
+import Chart from "/src/molecules/chart/Chart"
 
 const Page = () => {
 	const router = useRouter();
 	const { slug } = router.query
-
-	// Get chart-data (14 days)
 	const { data, loading, error } = useGet(`https://api.coingecko.com/api/v3/coins/${slug}/market_chart?vs_currency=usd&days=14&interval=daily`);
+
+	const [dataArray, setDataArray] = useState(null)
+
+	useEffect(()=>{
+		const mappedPriceArray = [];
+		data?.prices.map((price) => {
+			let priceOb = {
+				"date": price[0],
+				"price": price[1]
+			}
+			mappedPriceArray.push(priceOb)
+		})
+		setDataArray(mappedPriceArray)
+	}, [data])
 
 	return (
 		<Layout>
@@ -18,17 +34,25 @@ const Page = () => {
 				<title key="title">coin ghost</title>
 				<meta key="description" name="description" content="This is my project" />
 			</Head>
-			<h2>I am a detail-page of {slug}!</h2>
-			<h5>This is the historical market data for the 14-day-chart:</h5>
+
+			{/*Marquee component*/}
+			<Box sx={{ m: .75 }}>
+				<FastMarquee />
+			</Box>
+
 			{loading && <div>Loading...</div>}
 			{error && <div>{error.message}</div>}
-			{data && (
-				<pre>
-					<code>{JSON.stringify(data, null, 4)}</code>
-				</pre>
+			{dataArray && (
+				<>
+					<h5>Price chart for {slug}</h5>
+					{/* TODO: Change header-data based on tooltip-payload-change (tooltip) */}
+					{/*Chart component*/}
+					<Box sx={{ p: .5 }}>
+						<Chart dataArray={dataArray} />
+					</Box>
+				</>
 			)}
 		</Layout>
 	);
 };
-
 export default Page;

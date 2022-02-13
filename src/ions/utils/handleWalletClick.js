254f0cfbe42ff97cfd8beb1dd94dfbba75610be2
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-14 00:24:50 +0100

    fix: new-users-bug

--- src/ions/utils/handleWalletClick.js
+++ src/ions/utils/handleWalletClick.js
@@ -1,72 +1,62 @@
 import Web3 from "web3";
 import axios from "axios";
 
-
 const web3 = new Web3(Web3.givenProvider);
 
-
 // Signup helper-function
-const handleSignup = async (publicAddress) => {
+const handleSignup = publicAddress => {
 	// Add new user to data-base
-	await axios.post("/api/users/", { "publicAddress": `${publicAddress}` })
+	return axios.post("/api/users/", {"publicAddress": `${publicAddress}`})
 		.then(
 			response => {
-				console.log("New user added successfully:", response);
+				console.log("New user added successfully:", response)
+				return response
 			}
 		)
 		.catch(
 			error => {
-				console.error("Could not add new user:", error);
+				console.error("Could not add new user:", error)
 			}
 		);
-
-
 };
 
 // Sign-message helper-function
 const handleSignMessage = async (publicAddress) => {
-
 	// Retrieve nonce from database
 	console.log("Retrieving nonce from database")
-	console.log(publicAddress)
 	const nonce = await axios.get(`/api/users?publicAddress=${publicAddress}`)
 		.then(
 			response => {
 				console.log(response)
 				console.log(`Nonce found for ${publicAddress}:`, response.data[0].nonce)
+				return response.data[0].nonce
 			}
 		)
 		.catch(error => {
 			console.error(`Couldn't retrieve nonce for ${publicAddress}:`, error)
-
 		});
 
 	// Sign nonce
-	if (nonce){
-		return new Promise((resolve, reject) => {
-			console.log("Asking user to sign nonce:", nonce)
-			web3.eth.personal.sign(
-				`Please sign this message to confirm ownership of your public-address.\n
-					No transaction-cost occur.\n
-					Your personal code is ${nonce}.`,
-				publicAddress,
-				(err, signature) => {
-					if (err) return reject(err);
-					const userSignature = signature
-					console.log(signature)
-					return resolve({ publicAddress, userSignature});
-				}
-			)
-		})
-	}
+	return new Promise((resolve, reject) => {
+		console.log("Asking user to sign nonce:", nonce)
+		web3.eth.personal.sign(
+			`Please sign this message to confirm ownership of your public-address.\n
+				No transaction-cost occur.\n
+				Your personal code is ${nonce}.`,
+
+			publicAddress,
+			(err, signature) => {
+				if (err) return reject(err);
+				const userSignature = signature
+				console.log(signature)
+				return resolve({ publicAddress, userSignature});
+			}
+		)
+	})
 };
 
 // Handle authentication helper-function
-const handleAuthenticate = ({ publicAddress, userSignature }) => {
-	// Handle MetaMask not installed
-
-	// Verify signature
-	// Defining request-header
+const handleAuthenticate = async ({ publicAddress, userSignature}) => {
 	const options = {
 		headers: {},
 		data: {
@@ -90,60 +80,50 @@ const handleAuthenticate = ({ publicAddress, userSignature }) => {
 };
 
 const handleWalletClick = async () => {
+
 	// Allow site to connect to MetaMask
 	if(window.ethereum) {
 		try {
 			await ethereum.enable();
 		} catch (error) {
 			window.alert("You need to allow MetaMask.");
-			return;
 		}
 	} else if(!window.ethereum) {
 		window.open("https://metamask.io/download/");
-		return;
 	}
 
-	// Request public ethereum-address
-	const account = web3.eth.getAccounts((error,result) => {
-		if (error) {
-			console.error(".getAccounts failed:", error);
-		} else {
-			try {
-				const ethAddress = web3.currentProvider.selectedAddress;
-				// Check if ethereum-address is valid
-				console.log("Checking address")
+	// Request public ethereum-accounts
+	const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+	const account = accounts[0];
+	let publicAddress = null;
 
-				if(web3.utils.isAddress(ethAddress)) {
-					console.log("Valid ethAddress!", ethAddress)
+	// Check if selected account is valid and assign it
+	if(web3.utils.isAddress(account)){
+		console.log(`${account} is a valid eth-address!`)
+		publicAddress = account.toLowerCase();
+	}
 
-					// Set ethAddress to lower-case
-					const publicAddress = ethAddress.toLowerCase();
-
-					// Check if publicAddress already exists on back-end
-					axios.get(`/api/users?publicAddress=${publicAddress}`)
-						.then(response => {
-							// If yes, retrieve it, if no, create new user;
-							(response.data.length
-								? (console.log("User already registered:", response.data[0]))
-								: handleSignup(publicAddress))
-						})
-						// Popup MetaMask confirmation modal to sign message
-						.then(handleSignMessage(publicAddress)
-							// Send signature to back end on the /auth route
-							.then(handleAuthenticate)
-							.catch(
-								error => {
-									console.error(error)
-								}
-							)
-						)
+	// Check if publicAddress already exists on back-end
+	try {
+		await axios.get(`/api/users?publicAddress=${publicAddress}`)
+			.then(response => {
+				if(response.data.length){
+					handleSignMessage(publicAddress)
+						.then(({ publicAddress, userSignature}) => {
+							handleAuthenticate({ publicAddress, userSignature});
+						});
 				} else {
-					console.error("Invalid ethAddress:", ethAddress)
+					handleSignup(publicAddress)
+						.then(response => (handleSignMessage(publicAddress)))
+						.then(({ publicAddress, userSignature}) => {
+							handleAuthenticate({ publicAddress, userSignature});
+						});
 				}
-			} catch(error) {
-				console.error(error)
-			}
-		}
-	});}
+				}
+			)
+	} catch (error) {
+		console.error("Error fetching public address from database:", error)
+	}
+}
 
 export default handleWalletClick

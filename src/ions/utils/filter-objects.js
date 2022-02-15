a1c4971d114ab20139d628f7f24f17d877948cf6
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-16 00:49:14 +0100

    chore: delete file

--- src/ions/utils/filter-objects.js
+++
@@ -1,9 +0,0 @@
-const filterObject = (obj, filter, filterValue) =>
-	Object.keys(obj).reduce((acc, val) =>
-		(obj[val][filter] === filterValue ? acc : {
-				...acc,
-				[val]: obj[val]
-			}
-		), {});
-
-export default filterObject

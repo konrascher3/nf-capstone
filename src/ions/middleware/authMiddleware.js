59ce9cc9f63d82e21ba01187361eb92904409cdf
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-16 15:05:14 +0100

    feat: implement JWT-checker

---
+++ src/ions/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+
+import { config } from "/backend/src/config";
+import Favorites from "/src/ions/models/favorites.model";
+
+const protect = async (request, response) => {
+	if (request.headers.authorization && request.headers.authorization.startsWith("Bearer")) {
+		try {
+			// Get token from header; ("Bearer 3980r3756340798564327...")
+			const token = request.headers.authorization.split(" ")[1];
+
+			// Verify the token
+			const {
+				payload: { id },
+			} = await jwt.verify(token, config.secret);
+
+			// Get userId from token
+			request.userId = await Favorites.find({ ObjectId: id });
+			// next();
+		} catch (error) {
+			console.log("Could not verify token:", error);
+			response.status(401);
+		}
+	} else {
+		response.status(401).send("not authorized, no token");
+	}
+};
+
+export default protect;

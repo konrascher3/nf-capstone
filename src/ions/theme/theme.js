2c6bd40b81773906000f2846ab693a7d3a6fbba4
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-17 16:56:28 +0100

    chore: add main colors

--- src/ions/theme/theme.js
+++ src/ions/theme/theme.js
@@ -1,7 +1,26 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-	palette: {},
+	palette: {
+		mode: "dark",
+		common: {
+			black: "hsl(231, 15%, 18%)",
+			white: "hsl(60, 30%, 96%)",
+		},
+		primary: {
+			main: "hsl(326, 100%, 74%)",
+		},
+		secondary: {
+			main: "hsl(265, 89%, 78%)",
+		},
+		text: {
+			primary: "hsl(60, 30%, 96%)",
+		},
+		background: {
+			paper: "hsl(225, 27%, 31%)",
+			default: "hsl(225, 27%, 41%)",
+		},
+	},
 	typography: {
 		fontFamily: `"Roboto Condensed", Helvetica, Arial, sans-serif`,
 	},

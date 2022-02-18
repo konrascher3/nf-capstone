4ec22bd35621e21d36cd1a704cc142eb93300035
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-19 00:31:57 +0100

    feat: add theme-switch

--- src/molecules/sidebar-appbar/SidebarAppBar.jsx
+++ src/molecules/sidebar-appbar/SidebarAppBar.jsx
@@ -6,6 +6,7 @@ import AppBar from "@mui/material/AppBar";
 import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Box from "@mui/material/Box";
+import Switch from "@mui/material/Switch";
 
 // useStore
 import useStore from "/src/ions/hooks/state/useStore";
@@ -18,6 +19,8 @@ import { mdiClose } from "@mdi/js";
 const SidebarAppBar = () => {
 	const open = useStore(state => state.open);
 	const setOpen = useStore(state => state.setOpen);
+	const setColorMode = useStore(state => state.setColorMode);
+	const colorMode = useStore(state => state.colorMode);
 
 	return (
 		<Box sx={{ flexGrow: 1 }}>
@@ -26,16 +29,35 @@ const SidebarAppBar = () => {
 					sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}
 				>
 					<img style={{ width: "170px" }} src={logoDarkMode.src} alt="Coin ghost logo" />
-					<IconButton
-						color="inherit"
-						aria-label="open drawer"
-						edge="end"
-						onClick={() => {
-							setOpen(!open);
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+							gap: 1.75,
 						}}
 					>
-						<Icon path={mdiClose} size={1} />
-					</IconButton>
+						<Switch
+							defaultChecked
+							size="medium"
+							checked={colorMode}
+							onChange={() => {
+								setColorMode(!colorMode);
+							}}
+							sx={{}}
+						/>
+
+						<IconButton
+							color="inherit"
+							aria-label="open drawer"
+							edge="end"
+							onClick={() => {
+								setOpen(!open);
+							}}
+						>
+							<Icon path={mdiClose} size={1} />
+						</IconButton>
+					</Box>
 				</Toolbar>
 			</AppBar>
 		</Box>

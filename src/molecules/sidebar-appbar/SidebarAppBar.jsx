28aebe36813e560ed90dff426a60a597007e5502
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-20 20:22:02 +0100

    feat: add custom color-switch

--- src/molecules/sidebar-appbar/SidebarAppBar.jsx
+++ src/molecules/sidebar-appbar/SidebarAppBar.jsx
@@ -18,6 +18,8 @@ import logoLightMode from "/src/ions/img/logos/complete-logo-light-mode-optimize
 
 // Material-design-icons Imports
 import { mdiClose } from "@mdi/js";
+import { mdiWeatherNight } from "@mdi/js";
+import { mdiWhiteBalanceSunny } from "@mdi/js";
 
 const SidebarAppBar = () => {
 	const open = useStore(state => state.open);
@@ -45,13 +47,60 @@ const SidebarAppBar = () => {
 						}}
 					>
 						<Switch
-							// defaultChecked
 							size="medium"
+							sx={{
+								width: 62,
+								".MuiSwitch-switchBase": {
+									transform: "translate(-1px, -1.5px)",
+									"&.Mui-checked": {
+										transform: "translate(23px, -1.5px)",
+									},
+									"&& .MuiTouchRipple-child": {
+										backgroundColor: "blue",
+									},
+									// "& .MuiSwitch-thumb:before": {
+									// 	width: 32,
+									// 	height: 32,
+									// },
+								},
+							}}
 							checked={colorMode}
+							checkedIcon={
+								<Box
+									sx={{
+										boxShadow: 1,
+										p: 0.3,
+										borderRadius: 100,
+										color: "hsl(225, 27%, 31%)",
+										backgroundColor: "hsl(326, 100%, 74%)",
+										display: "flex",
+										justifyContent: "center",
+										alignItems: "center",
+									}}
+								>
+									<Icon path={mdiWeatherNight} size={0.8} />
+								</Box>
+							}
+							icon={
+								<Box
+									sx={{
+										// transform: "translateY(-3px)",
+										boxShadow: 1,
+										p: 0.3,
+										borderRadius: 100,
+										color: "hsl(225, 27%, 31%)",
+										backgroundColor: "hsl(60, 30%, 96%)",
+										display: "flex",
+										justifyContent: "center",
+										// alignItems: "center",
+									}}
+								>
+									<Icon path={mdiWhiteBalanceSunny} size={0.8} />
+								</Box>
+							}
 							onChange={() => {
 								setColorMode(colorMode);
 							}}
-							sx={{}}
 						/>
 
 						<IconButton

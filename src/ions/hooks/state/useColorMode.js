8839feb3b46dd254ec8471a7f66f065914797be7
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-22 16:16:33 +0100

    feat: persist color mode

--- src/ions/hooks/state/useColorMode.js
+++ src/ions/hooks/state/useColorMode.js
@@ -1,8 +1,17 @@
 import create from "zustand";
+import { persist } from "zustand/middleware";
 
-const useStore = create(set => ({
-	colorMode: true,
-	setColorMode: boolean => set({ colorMode: !boolean }),
-}));
+const useStore = create(
+	persist(
+		(set, get) => ({
+			colorMode: true,
+			toggleColorMode: () => {
+				const colorMode = get().colorMode;
+				set({ colorMode: !colorMode });
+			},
+		}),
+		{ name: "coin-ghost-colorMode" }
+	)
+);
 
 export default useStore;

c994ca0a4916591ee3070b69f31e4d19abaf5808
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-23 13:22:32 +0100

    chore: add pwa-configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 require("dotenv").config();
 /**
  * @type {import('next').NextConfig}
@@ -13,6 +14,14 @@ const nextConfig = {
 		JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
 		API_ROUTE_KEY: process.env.API_ROUTE_KEY,
 	},
+	pwa: {
+		runtimeCaching,
+		buildExcludes: [/middleware-manifest.json$/],
+		dest: "public",
+		register: true,
+		skipWaiting: true,
+		disable: process.env.NODE_ENV === "development",
+	},
 };
 
-module.exports = withPWA({ nextConfig });
+module.exports = withPWA(nextConfig);

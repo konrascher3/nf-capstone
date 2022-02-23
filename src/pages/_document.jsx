f8e454273ae92e64ea03062a43ffa2e150351af1
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-23 21:37:15 +0100

    fix: add fav-icons

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -136,8 +136,10 @@ class MyDocument extends Document {
 						href="assets/apple-splash-1136-640.jpg"
 						media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
 					/>
-
+					<link rel="shortcut icon" href="/favicon.ico" />
 					<link rel="apple-touch-icon" href="assets/apple-icon-180.png" />
+					<link rel="icon" type="image/png" sizes="32x32" href="/icon-32x32.png" />
+					<link rel="icon" type="image/png" sizes="16x16" href="/icon-16x16.png" />
 					<meta name="apple-mobile-web-app-capable" content="yes" />
 					<link rel="manifest" href="/manifest.json" />
 					<meta name="theme-color" content="#3a4464" />
@@ -151,15 +153,7 @@ class MyDocument extends Document {
 					<meta name="mobile-web-app-capable" content="yes" />
 					<meta name="robots" content="noindex,nofollow" />
 					<meta name="msapplication-tap-highlight" content="no" />
-					{/*<link*/}
-					{/*	rel="apple-touch-icon"*/}
-					{/*	sizes="180x180"*/}
-					{/*	href="/icons/apple-touch-icon.png"*/}
-					{/*/>*/}
-					{/*<link rel="icon" type="image/png" sizes="32x32" href="/icons/icon-32x32.png" />*/}
-					{/*<link rel="icon" type="image/png" sizes="16x16" href="/icons/icon-16x16.png" />*/}
 					<link rel="manifest" href="/manifest.json" />
-					{/*<link rel="shortcut icon" href="/favicon.ico" />*/}
 				</Head>
 				<body>
 					<Main />

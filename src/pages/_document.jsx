453a1269ce6dd4eb10c9133ed9555df301a01ca9
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-24 04:23:48 +0100

    chore: add meta-tags for pwa

---
+++ src/pages/_document.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link rel="manifest" href="/manifest.json" />
+					<meta name="theme-color" content="#3a4464" />
+					<meta charSet="utf-8" />
+					<meta name="description" content="Track your favorite coins with coin ghost" />
+					<meta name="apple-mobile-web-app-capable" content="yes" />
+					<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+					<meta name="apple-mobile-web-app-title" content="coin ghost" />
+					<meta name="format-detection" content="telephone=no" />
+					<meta name="mobile-web-app-capable" content="yes" />
+					<meta name="robots" content="noindex,nofollow" />
+					<meta name="msapplication-tap-highlight" content="no" />
+					<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;

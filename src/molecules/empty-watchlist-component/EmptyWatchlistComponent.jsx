af1321005e338ba870fd127b17afa8528624f6ef
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-24 04:45:39 +0100

    chore: refactor EmptyWatchlistComponent.jsx

--- src/molecules/empty-watchlist-component/EmptyWatchlistComponent.jsx
+++ src/molecules/empty-watchlist-component/EmptyWatchlistComponent.jsx
@@ -2,29 +2,35 @@ import React from "react";
 
 // MUI Import
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 // Custom-components Imports
 import LottieTumbleweed from "/src/atoms/lottie-tumbleweed/LottieTumbleweed";
-
-import useStore from "/src/ions/hooks/state/useStore";
 import useColorMode from "/src/ions/hooks/state/useColorMode";
-
 import { darkMode, lightMode } from "/src/ions/theme/theme";
 
 const EmptyWatchlistComponent = () => {
 	const colorMode = useColorMode(state => state.colorMode);
 	return (
-		<>
+		<Box
+			sx={{
+				width: "100vw",
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
 			<LottieTumbleweed />
 			<Typography
 				variant="h4"
 				color={colorMode ? darkMode.palette.text.primary : lightMode.palette.text.primary}
-				sx={{ textAlign: "center", margin: "0 auto" }}
-				maxWidth={200}
+				sx={{ textAlign: "center" }}
+				maxWidth={400}
 			>
 				It looks kinda empty here...
 			</Typography>
-		</>
+		</Box>
 	);
 };
 

9bfb8367b8b1b3c5b587ecc086b29f82f20b9b9e
Konrad Scherb <konrad.scherb@gmx.de>
2022-02-24 05:36:19 +0100

    chore: add fav-icons

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -6,6 +6,25 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/images/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/images/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/images/favicon-16x16.png"
+					/>
+					<link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#3a4464" />
+
 					<link rel="manifest" href="/manifest.json" />
 					<meta name="theme-color" content="#3a4464" />
 					<meta charSet="utf-8" />
